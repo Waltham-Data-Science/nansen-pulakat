dcc65f338669867817c171fc90e7d4e37919d37a
Jess Haley <jess@walthamdatascience.com>
2025-12-11 14:30:04 -0800

    Create newDataset.m

---
+++ pulakat/code-NDI/+pulakat/newDataset.m
@@ -0,0 +1,34 @@
+function [dataset,cloudDatasetID] = newDataset(datasetName,dataPath,cloud)
+%SETUP Summary of this function goes here
+
+% Input argument validation
+arguments
+    datasetName {mustBeText}
+    dataPath {mustBeFolder} = fullfile(userpath,'Datasets');
+    cloud (1,1) logical = true; % automatically add dataset to cloud
+end
+
+% Define dataset directory path
+datasetDir = fullfile(dataPath,datasetName);
+
+% Create directory (if needed)
+if ~exist(datasetDir,'dir')
+    mkdir(datasetDir);
+end
+
+% Create dataset
+dataset = ndi.dataset.dir(datasetName,datasetDir);
+
+% Add dataset to cloud
+if cloud
+    [success, cloudDatasetID, message] = ndi.cloud.uploadDataset(dataset, ...
+        'skipMetadataEditorMetadata',true, ...
+        'remoteDatasetName',datasetName,'Verbose',true);
+    if ~success
+        disp(message)
+    else
+        sprintf('Dataset %s added to the cloud as %s',datasetName,cloudDatasetID);
+    end
+end
+
+end
