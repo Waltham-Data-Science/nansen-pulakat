8062d4f47099cd7a6acbf2bc048c564c1c55baf4
Jess Haley <jess@walthamdatascience.com>
2026-01-12 15:22:45 -0800

    Update startup.m

--- pulakat/code/+pulakat/startup.m
+++ pulakat/code/+pulakat/startup.m
@@ -48,25 +48,40 @@ end
 
 % 3. Update nansen project from GitHub
 
-% Clone or pull changes from github repo
-startupPath = mfilename('fullpath');
-nansenRepoPath = fullfile(codePath,'nansen-pulakat');
-if ~isfolder(nansenRepoPath)
-    % Clone project repo from github
-    repoURL = 'https://github.com/Waltham-Data-Science/nansen-pulakat';
-    repo = gitclone(repoURL,nansenRepoPath);
+% Ask Git for the root of the repository
+[currentDir, ~, ~] = fileparts(mfilename('fullpath'));
+cmd = sprintf('git -C "%s" rev-parse --show-toplevel', currentDir);
+[status, cmdOut] = system(cmd);
+if status == 0
+    repoPath = strtrim(cmdOut); % strtrim removes the newline character
 else
-    % Pull changes to project from github
-    repo = gitrepo(nansenRepoPath);
-    pull(repo);
+    % If not a git repo, fall back to the current folder or throw error
+    error('The current function is not inside a Git repository.');
 end
+
+% Pull changes from github repo
+fprintf('Checking for updates in: %s\n', repoPath);
+pullCmd = sprintf('git -C "%s" pull', repoPath);
+[status, cmdOut] = system(pullCmd);
+if status == 0
+    if contains(cmdOut, 'Already up to date')
+        fprintf('Your repository is already up to date.\n');
+    else
+        fprintf('Updates applied successfully:\n%s\n', cmdOut);
+    end
+else
+    % Common errors: No internet, merge conflicts, or uncommitted changes
+    warning('Failed to pull updates. Git message:\n%s', cmdOut);
+end
+
+% Switch branch
 if strcmp(repo.CurrentBranch.Name,'main') % REMOVE LATER!!!!
     switchBranch(repo,'update-metatable');
 end
 
 % Load pulakat project from nansen project manager
 projectName = 'pulakat';
-projectPath = fullfile(nansenRepoPath,projectName);
+projectPath = fullfile(repoPath,projectName);
 projectManager = nansen.ProjectManager(); 
 
 % Import the project from the repo if that hasn't already been done
