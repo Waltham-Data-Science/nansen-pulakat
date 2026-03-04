569062730000262cf1acf365df4a16bb3d007209
ehennestad <ehennestad@gmail.com>
2026-03-04 14:12:13 +0100

    Update startup.m
    Create ndi root data folder if it does not exist

--- pulakat/code/+pulakat/startup.m
+++ pulakat/code/+pulakat/startup.m
@@ -2,7 +2,11 @@ function [] = startup()
 %STARTUP Summary of this function goes here
 %   Detailed explanation goes here
 
+dataDir = fullfile(userpath, 'ndi', 'data');
+if ~isfolder(dataDir)
+    mkdir(dataDir)
+end
+
 ndi.nansen.startup('pulakat');
 
 end
-
