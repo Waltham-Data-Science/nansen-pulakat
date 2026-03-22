b157f5ad2d9444229ef16673937a259cf399e7ab
Jess Haley <jess@walthamdatascience.com>
2026-03-22 16:52:49 -0700

    renamed files to file

--- pulakat/code/+pulakat/+sessionmethod/+methods/+import/+data/file.m
+++ pulakat/code/+pulakat/+sessionmethod/+methods/+import/+data/file.m
@@ -1,9 +1,9 @@
-function varargout = files(sessionObject, varargin)
-%AUTO Imports subjects for a session into NDI and updates the metatable.
+function varargout = file(sessionObject, varargin)
+%MANUAL Manually adds data files for a session into NDI and updates the metatable.
 %
-%   This session method identifies subjects associated with the session,
-%   imports them into the NDI database, checks their cloud synchronization
-%   status, and updates the session's subject and session metatables.
+%   This session method allows the user to manually select files and
+%   specify their metadata, imports them into the NDI database, checks
+%   their cloud synchronization status, and updates the session's file metatable.
 %
 %   Inputs:
 %       sessionObject (nansen.session.Session): The Nansen session object.
@@ -14,36 +14,38 @@ function varargout = files(sessionObject, varargin)
 
     % Get struct of parameters from local function
     params = getDefaultParameters();
-    
+
     % Create a cell array with attribute keywords
     ATTRIBUTES = {'serial', 'queueable'};
-    
+
     % Create a struct with "attributes" using a predefined pattern
     import nansen.session.SessionMethod
     fcnAttributes = SessionMethod.setAttributes(params, ATTRIBUTES{:});
-    
+
     if ~nargin && nargout > 0
         varargout = {fcnAttributes};   return
     end
-    
+
     % Parse name-value pairs from function input and update parameters
     params = utility.parsenvpairs(params, [], varargin);
-    
+
     % --- Implementation of the method ---
 
     % Get dataset and session objects
     dataset = ndi.nansen.fun.datasetID2Object(sessionObject.DatasetIdentifier);
     session = ndi.session.dir(sessionObject.SessionPath);
 
-    % Add subjects to session
-    subjectTable = ndi.nansen.import.subject.auto(session);
+    % Add files to session
+    dataTable = ndi.nansen.import.file.auto(session);
 
-    if isempty(subjectTable); return; end
+    if isempty(dataTable); return; end
 
-    % Update subject and session metatables
-    ndi.nansen.metatable.update(dataset,'Subject');
+    % Update metatables
+    ndi.nansen.metatable.update(dataset,'Subject'); % file needs to inheret info from new subjects
+    ndi.nansen.metatable.update(dataset,'File');
+    ndi.nansen.metatable.update(dataset,'Subject','UpdateVariableNames',{'NumFiles','DataTypeName'}); % subject needs # files & datatypes
     ndi.nansen.metatable.update(dataset,'Session');
-    
+
     % Return session object (please do not remove):
     % if nargout; varargout = {sessionObject}; end
 end
--- pulakat/code/+pulakat/+sessionmethod/+methods/+import/+subjects/file.m
+++ pulakat/code/+pulakat/+sessionmethod/+methods/+import/+subjects/file.m
@@ -1,9 +1,9 @@
-function varargout = files(sessionObject, varargin)
-%MANUAL Manually adds data files for a session into NDI and updates the metatable.
+function varargout = file(sessionObject, varargin)
+%AUTO Imports subjects for a session into NDI and updates the metatable.
 %
-%   This session method allows the user to manually select files and
-%   specify their metadata, imports them into the NDI database, checks
-%   their cloud synchronization status, and updates the session's file metatable.
+%   This session method identifies subjects associated with the session,
+%   imports them into the NDI database, checks their cloud synchronization
+%   status, and updates the session's subject and session metatables.
 %
 %   Inputs:
 %       sessionObject (nansen.session.Session): The Nansen session object.
@@ -14,38 +14,36 @@ function varargout = files(sessionObject, varargin)
 
     % Get struct of parameters from local function
     params = getDefaultParameters();
-
+    
     % Create a cell array with attribute keywords
     ATTRIBUTES = {'serial', 'queueable'};
-
+    
     % Create a struct with "attributes" using a predefined pattern
     import nansen.session.SessionMethod
     fcnAttributes = SessionMethod.setAttributes(params, ATTRIBUTES{:});
-
+    
     if ~nargin && nargout > 0
         varargout = {fcnAttributes};   return
     end
-
+    
     % Parse name-value pairs from function input and update parameters
     params = utility.parsenvpairs(params, [], varargin);
-
+    
     % --- Implementation of the method ---
 
     % Get dataset and session objects
     dataset = ndi.nansen.fun.datasetID2Object(sessionObject.DatasetIdentifier);
     session = ndi.session.dir(sessionObject.SessionPath);
 
-    % Add files to session
-    dataTable = ndi.nansen.import.file.auto(session);
+    % Add subjects to session
+    subjectTable = ndi.nansen.import.subject.auto(session);
 
-    if isempty(dataTable); return; end
+    if isempty(subjectTable); return; end
 
-    % Update metatables
-    ndi.nansen.metatable.update(dataset,'Subject'); % file needs to inheret info from new subjects
-    ndi.nansen.metatable.update(dataset,'File');
-    ndi.nansen.metatable.update(dataset,'Subject','UpdateVariableNames',{'NumFiles','DataTypeName'}); % subject needs # files & datatypes
+    % Update subject and session metatables
+    ndi.nansen.metatable.update(dataset,'Subject');
     ndi.nansen.metatable.update(dataset,'Session');
-
+    
     % Return session object (please do not remove):
     % if nargout; varargout = {sessionObject}; end
 end
