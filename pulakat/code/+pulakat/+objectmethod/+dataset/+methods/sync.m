588ab61b575a2e2e5d7e900ca8ed563d0cb7d9c6
Jess Haley <jess@walthamdatascience.com>
2026-03-25 08:19:30 -0700

    Dataset sync updates

--- pulakat/code/+pulakat/+objectmethod/+dataset/+methods/sync.m
+++ pulakat/code/+pulakat/+objectmethod/+dataset/+methods/sync.m
@@ -33,124 +33,6 @@ function varargout = sync(datasetObject, varargin)
     % Get dataset object
     dataset = ndi.dataset.dir(datasetObject.DatasetPath);
 
-    % Get Nansen project
-    project = nansen.getCurrentProject;
-    labName = project.Name;
-
-    % Get project info
-    projectFile = fullfile('+ndi','+setup','+conv',['+',labName],'project_info.json');
-    projectInfo = jsondecode(fileread(projectFile));
-    subjectIdentifiers = projectInfo.subjectIdentifierFields;
-
-    % 1. Create NDI Session documents if missing
-    sessionMetaTable = project.MetaTableCatalog.getMetaTable('Session');
-    sessionTable = sessionMetaTable.entries;
-
-    for i = 1:height(sessionTable)
-        if isempty(sessionTable.SessionDocumentIdentifier{i})
-            sessionPath = sessionTable.SessionPath{i};
-            sessionName = sessionTable.SessionName{i};
-            [dataParentDir,sessionFolderName] = fileparts(sessionPath);
-
-            SessionRef = {sessionName};
-            SessionPath = {sessionFolderName};
-            sessionMaker = ndi.setup.NDIMaker.sessionMaker(dataParentDir,...
-                table(SessionRef,SessionPath));
-            session = sessionMaker.sessionIndices{1};
-
-            % Add to dataset
-            [~,sessionIDs] = dataset.session_list;
-            if ~any(strcmp(sessionIDs,session.id))
-                dataset.add_linked_session(session);
-            end
-
-            % Get session document id
-            query = ndi.query('base.session_id','exact_string',session.id);
-            doc = session.database_search(query);
-            if ~isempty(doc)
-                sessionTable.SessionDocumentIdentifier{i} = doc{1}.document_properties.base.id;
-                sessionTable.SessionIdentifier{i} = session.id;
-            else
-                warning('Could not find NDI document for session: %s', sessionName);
-            end
-        end
-    end
-    ndi.nansen.metatable.add(sessionTable, 'Session');
-
-    % 2. Create NDI Subject documents if missing
-    subjectMetaTable = project.MetaTableCatalog.getMetaTable('Subject');
-    subjectTable = subjectMetaTable.entries;
-    if ~isempty(subjectTable)
-        indPending = cellfun(@(x) isempty(x) || strcmp(x, 'N/A'), subjectTable.SubjectDocumentIdentifier);
-    else
-        indPending = false(0,1);
-    end
-
-    if any(indPending)
-        pendingSubjects = subjectTable(indPending, :);
-        uniqueSessions = unique(pendingSubjects.SessionIdentifier);
-        for i = 1:numel(uniqueSessions)
-            if isempty(uniqueSessions{i}); continue; end
-            session = dataset.open_session(uniqueSessions{i});
-            indSess = strcmp(pendingSubjects.SessionIdentifier, uniqueSessions{i});
-            createdSubjects = ndi.nansen.import.subject.createDocuments(session, pendingSubjects(indSess, :), labName);
-
-            % Update main table
-            for j = 1:height(createdSubjects)
-                indSubjMatch = true(height(subjectTable),1);
-                for f = 1:numel(subjectIdentifiers)
-                    indSubjMatch = indSubjMatch & strcmp(subjectTable.(subjectIdentifiers{f}), createdSubjects.(subjectIdentifiers{f}){j});
-                end
-                ind = strcmp(subjectTable.SessionIdentifier, uniqueSessions{i}) & indSubjMatch;
-                subjectTable.SubjectDocumentIdentifier(ind) = createdSubjects.SubjectDocumentIdentifier(j);
-                subjectTable.SubjectLocalIdentifier(ind) = createdSubjects.SubjectLocalIdentifier(j);
-            end
-        end
-        ndi.nansen.metatable.add(subjectTable, 'Subject');
-    end
-
-    % 3. Create NDI File documents if missing
-    fileMetaTable = project.MetaTableCatalog.getMetaTable('File');
-    fileTable = fileMetaTable.entries;
-
-    if ~isempty(fileTable)
-        % Update SubjectDocumentIdentifier and SubjectLocalIdentifier in fileTable from subjectTable
-        [Lia, indS] = ismember(fileTable(:, [{'SessionIdentifier'}, subjectIdentifiers']), ...
-            subjectTable(:, [{'SessionIdentifier'}, subjectIdentifiers']));
-
-        fileTable.SubjectDocumentIdentifier(Lia) = subjectTable.SubjectDocumentIdentifier(indS(Lia));
-        fileTable.SubjectLocalIdentifier(Lia) = subjectTable.SubjectLocalIdentifier(indS(Lia));
-
-        indPending = cellfun(@(x) isempty(x) || strcmp(x, 'N/A'), fileTable.FileDocumentIdentifier);
-    else
-        indPending = false(0,1);
-    end
-    if any(indPending)
-        pendingFiles = fileTable(indPending, :);
-        uniqueSessions = unique(pendingFiles.SessionIdentifier);
-        for i = 1:numel(uniqueSessions)
-            session = dataset.open_session(uniqueSessions{i});
-            indSess = strcmp(pendingFiles.SessionIdentifier, uniqueSessions{i});
-            createdFiles = ndi.nansen.import.file.createDocuments(session, pendingFiles(indSess, :), labName);
-
-            % Update main table
-            for j = 1:height(createdFiles)
-                % Match using identifying fields since SubjectLocalIdentifier might have just been created
-                indSubjMatch = true(height(fileTable),1);
-                for f = 1:numel(subjectIdentifiers)
-                    indSubjMatch = indSubjMatch & strcmp(fileTable.(subjectIdentifiers{f}), createdFiles.(subjectIdentifiers{f}){j});
-                end
-
-                ind = strcmp(fileTable.SessionIdentifier, uniqueSessions{i}) & ...
-                      indSubjMatch & ...
-                      strcmp(fileTable.ElectronicFileName, createdFiles.ElectronicFileName{j}) & ...
-                      strcmp(fileTable.DataTypeName, createdFiles.DataTypeName{j});
-                fileTable.FileDocumentIdentifier(ind) = createdFiles.FileDocumentIdentifier(j);
-            end
-        end
-        ndi.nansen.metatable.add(fileTable, 'File');
-    end
-
     % Sync dataset to cloud
     success = ndi.cloud.sync.uploadNew(dataset);
     if ~success
@@ -163,9 +45,7 @@ function varargout = sync(datasetObject, varargin)
     end
 
     % Update metatable
-    datasetTable = ndi.nansen.metatable.dataset(dataset);
-    ndi.nansen.metatable.remove(datasetTable,'Dataset');
-    ndi.nansen.metatable.add(datasetTable,'Dataset');
+    ndi.nansen.metatable.update(dataset,'Dataset');
     
     % Return session object (please do not remove):
     % if nargout; varargout = {datasetObject}; end
